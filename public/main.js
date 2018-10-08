23a0c6eec19f68021bcaad0a52b3cd5545906102
Angel Rodriguez <angelr1076@gmail.com>
2018-10-07 20:31:40 -0400

    fixed javascript charCount on all pages

--- public/main.js
+++ public/main.js
@@ -1,26 +1,13 @@
-
-let textArea = document.querySelector("#bioInput");
-let charEntered = document.querySelector(".header");
+// let textArea = document.querySelector("#textArea");
+let textArea = document.getElementById("textArea");
+let charEntered = document.getElementById("postCount");
 
 function keyDown(e) {
     let length = textArea.value.length;
-    charEntered.innerText = `(${150 - length} characters remaining)`;
+    charEntered.innerText = (150 - length) + " characters remaining";
 
-    if (length > 150) {
+    if (length >= 150) {
         event.preventDefault();
-        charEntered.innerText = "(Maximum characters have been reached)";
+        charEntered.innerText = "Maximum characters have been reached";
     }
-} 
-
-let postSize = document.querySelector("#postArea");
-let numChar = document.querySelector(".postCount");
-
-function charCount(e) {
-    let length = postSize.value.length;
-    numChar.innerText = `(${150 - length} characters remaining)`;
-
-    if (length > 150) {
-        event.preventDefault();
-        numChar.innerText = "(Maximum characters have been reached)";
-    }
-} 
+}
